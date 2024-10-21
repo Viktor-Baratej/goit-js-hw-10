1f6790bfb11f5f22880cc455f110936217115a0c
Viktor-Baratej <Viktor_baratejj@ukr.net>
2024-10-21 20:31:40 +0000

    Deploying to gh-pages from @ Viktor-Baratej/goit-js-hw-10@a789615b79c8ef5641e61f33ff8b9b2389f4334a 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{i}from"./assets/vendor-BbbuE1sJ.js";const s=document.querySelector(".form");s.addEventListener("submit",function(o){o.preventDefault();const t=parseInt(s.delay.value),r=s.state.value;new Promise((e,m)=>{setTimeout(()=>{r==="fulfilled"?e(t):m(t)},t)}).then(e=>{i.success({title:"Success",message:`✅ Fulfilled promise in ${e}ms`,timeout:3e3})}).catch(e=>{i.error({title:"Error",message:`❌ Rejected promise in ${e}ms`,timeout:3e3})})});
+//# sourceMappingURL=2-snackbar.js.map
